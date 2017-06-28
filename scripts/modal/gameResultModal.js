9d10e92ee15c4ed29c1b1cee3295f2d78a2c443f
Tien Ho <thuytien140894@gmail.com>
2017-06-28 16:49:57 -0400

    Added modal classes and refactored site.css

---
+++ scripts/modal/gameResultModal.js
@@ -0,0 +1,25 @@
+fChess = fChess || {};
+
+fChess.GameResultModal = (function () {
+    'use strict';
+
+    var GameResultModal = function () {
+    };
+
+    //fields
+
+    //functions
+    GameResultModal.prototype.show = function () {
+
+    };
+
+    GameResultModal.prototype.close = function () {
+
+    };
+
+    GameResultModal.prototype.confirm = function () {
+
+    };
+
+    return GameResultModal;
+})();
---
+++ scripts/modal/newGameModal.js
@@ -0,0 +1,25 @@
+fChess = fChess || {};
+
+fChess.NewGameModal = (function () {
+    'use strict';
+
+    var NewGameModal = function () {
+    };
+
+    //fields
+
+    //functions
+    NewGameModal.prototype.show = function () {
+
+    };
+
+    NewGameModal.prototype.close = function () {
+
+    };
+
+    NewGameModal.prototype.confirm = function () {
+
+    };
+
+    return NewGameModal;
+})();
